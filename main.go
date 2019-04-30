884e6b20ef38e8d66e34f5ca695c23b73f4e0b6a
ben <chocolatey030@gmail.com>
2019-04-30 02:15:48 +0200

    [GH-6] overwrite default user and password

--- main.go
+++ main.go
@@ -9,10 +9,15 @@ import (
 
 func main() {
 	nexus3URL := flag.String("nexus3URL", "http://localhost:8081", "The Nexus3URL")
+	nexus3user := flag.String("nexus3user", "admin", "The Nexus user")
+	nexus3pass := flag.String("nexus3pass", "admin123", "The Nexus password")
+
 	flag.Parse()
 
-	n := cli.Nexus3{URL: *nexus3URL}
+	n := cli.Nexus3{URL: *nexus3URL, User: *nexus3user, Pass: *nexus3pass}
 	log.Info("Nexus3URL: " + n.URL)
+	log.Info("Nexus3user: " + n.User)
+	log.Info("Nexus3pass: ****")
 
 	n.StoreArtifactsOnDisk()
 }
