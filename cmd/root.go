6a6f3ac4d317385672c6715b92d9443cf126f98e
030 <chocolatey030@gmail.com>
2019-05-15 04:19:17 +0200

    [GH-8] removed not implemented upload subcommand from readme

--- cmd/root.go
+++ cmd/root.go
@@ -28,13 +28,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "n3dr",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Nexus3 Disaster Recovery (N3DR)",
+	Long: `N3DR is a tool that is able to download all artifacts from
+a certain Nexus3 repository.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -55,11 +51,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.n3dr.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.n3dr.yaml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
