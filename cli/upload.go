0f489f596e755d7bc0cd23fb92b55ba6dac3563e
ben <chocolatey030@gmail.com>
2019-05-26 18:16:46 +0200

    [GH-7] removed commented out code

--- cli/upload.go
+++ cli/upload.go
@@ -20,30 +20,6 @@ func (n Nexus3) detectFoldersWithPOM(path string, f os.FileInfo, err error) erro
 
 // Upload posts an artifact as a multipart to a specific nexus3 repository
 func (n Nexus3) Upload() error {
-	// path := filepath.Join("testFiles", "file1")
-	// url := n.URL + "/service/rest/v1/components?repository=" + n.Repository
-	// u := mp.Upload{URL: url, Username: n.User, Password: n.Pass}
-	// err := u.MultipartUpload("maven2.asset1=@" + path + ".pom,maven2.asset1.extension=pom,maven2.asset2=@" + path + ".jar,maven2.asset2.extension=jar")
-	// if err != nil {
-	// 	return err
-	// }
-
-	// files, err := ioutil.ReadDir(n.Repository)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// for _, f := range files {
-	// 	fmt.Println(f)
-	// }
-
-	// err := filepath.Walk(n.Repository, n.multipart)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// fmt.Println(strings.TrimSuffix(s.String(), ","))
-
 	err2 := filepath.Walk(n.Repository, n.detectFoldersWithPOM)
 	if err2 != nil {
 		return err2
