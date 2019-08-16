3b6c9197b792184fb9dd68ffb963d870eef4d118
ben <chocolatey030@gmail.com>
2019-08-17 00:13:58 +0200

    [GH-44] broken integration test 3.9.0

--- integration-tests.sh
+++ integration-tests.sh
@@ -49,15 +49,15 @@ upload(){
 backup(){
     echo "Testing backup..."
     $TOOL backup -n http://localhost:9999 -u admin -p $PASSWORD -r maven-releases -v ${NEXUS_API_VERSION}
+    $TOOL backup -n http://localhost:9999 -u admin -p $PASSWORD -r maven-releases -v ${NEXUS_API_VERSION} -z
 
     if [ "${NEXUS_VERSION}" == "3.9.0" ]; then
         count_downloads 15
+        test_zip 12
     else
         count_downloads 63
+        test_zip 24
     fi
-
-    $TOOL backup -n http://localhost:9999 -u admin -p $PASSWORD -r maven-releases -v ${NEXUS_API_VERSION} -z
-    test_zip 24
 
     cleanup_downloads
 }
@@ -67,15 +67,15 @@ repositories(){
     $TOOL repositories -n http://localhost:9999 -u admin -p $PASSWORD -v ${NEXUS_API_VERSION} -a | grep maven-releases
     $TOOL repositories -n http://localhost:9999 -u admin -p $PASSWORD -v ${NEXUS_API_VERSION} -c | grep 7
     $TOOL repositories -n http://localhost:9999 -u admin -p $PASSWORD -v ${NEXUS_API_VERSION} -b
+    $TOOL repositories -n http://localhost:9999 -u admin -p $PASSWORD -v ${NEXUS_API_VERSION} -b -z
 
     if [ "${NEXUS_VERSION}" == "3.9.0" ]; then
         count_downloads 30
+        test_zip 20
     else
         count_downloads 126
+        test_zip 48
     fi
-
-    $TOOL repositories -n http://localhost:9999 -u admin -p $PASSWORD -v ${NEXUS_API_VERSION} -b -z
-    test_zip 48
 
     cleanup_downloads
 }
