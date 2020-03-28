1bf97fd2499ec0b0ff6bf7ea5243bb5a9edd83d4
030 <chocolatey030@gmail.com>
2020-03-28 21:41:45 +0100

    [GH-113] code smells

--- cli/upload.go
+++ cli/upload.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	mp "github.com/030/go-multipart/utils"
@@ -39,60 +40,72 @@ func (n Nexus3) detectFoldersWithPOM(d string) error {
 	return nil
 }
 
+func sbArtifact(path string, ext string, number string) string {
+	log.Debug(ext + " found " + path)
+	return "maven2.asset" + number + "=@" + path + ",maven2.asset" + number + ".extension=" + ext + ","
+}
+
+func artifactTypeDetector(path string, sb strings.Builder) strings.Builder {
+	switch ext := filepath.Ext(path); ext {
+	case ".pom":
+		sb.WriteString(sbArtifact(path, "pom", "1"))
+	case ".jar":
+		sb.WriteString(sbArtifact(path, "jar", "2"))
+	case ".war":
+		sb.WriteString(sbArtifact(path, "war", "3"))
+	case ".zip":
+		sb.WriteString(sbArtifact(path, "zip", "42"))
+	default:
+		log.Debug(path + " not an artifact")
+	}
+	return sb
+}
+
+func (n Nexus3) multipartUpload(sb strings.Builder) error {
+	multipartString := strings.TrimSuffix(sb.String(), ",")
+	url := n.URL + "/service/rest/" + n.APIVersion + "/components?repository=" + n.Repository
+	log.WithFields(log.Fields{
+		"multipart": multipartString,
+		"url":       url,
+	}).Debug("URL")
+
+	u := mp.Upload{URL: url, Username: n.User, Password: n.Pass}
+	if err := u.MultipartUpload(multipartString); err != nil {
+		return err
+	}
+	return nil
+}
+
+func pomDirs(p string) (strings.Builder, error) {
+	var sb strings.Builder
+	err := filepath.Walk(p, func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !f.IsDir() {
+			sb = artifactTypeDetector(path, sb)
+		}
+		return nil
+	})
+	if err != nil {
+		return sb, err
+	}
+	return sb, nil
+}
+
 // Upload posts an artifact as a multipart to a specific nexus3 repository
 func (n Nexus3) Upload() error {
 	if err := n.detectFoldersWithPOM(n.Repository); err != nil {
 		return err
 	}
-
-	for _, v := range foldersWithPOMStringSlice {
-		var s strings.Builder
-		err := filepath.Walk(v, func(path string, f os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if !f.IsDir() {
-				if filepath.Ext(path) == ".pom" {
-					log.Debug("POM found " + path)
-					s.WriteString("maven2.asset1=@" + path + ",")
-					s.WriteString("maven2.asset1.extension=pom,")
-				}
-
-				if filepath.Ext(path) == ".jar" {
-					log.Debug("JAR found " + path)
-					s.WriteString("maven2.asset2=@" + path + ",")
-					s.WriteString("maven2.asset2.extension=jar,")
-				}
-
-				if filepath.Ext(path) == ".war" {
-					log.Debug("WAR found " + path)
-					s.WriteString("maven2.asset3=@" + path + ",")
-					s.WriteString("maven2.asset3.extension=war,")
-				}
-
-				if filepath.Ext(path) == ".zip" {
-					log.Debug("ZIP found " + path)
-					s.WriteString("maven2.asset42=@" + path + ",")
-					s.WriteString("maven2.asset42.extension=zip,")
-				}
-			}
-			return nil
-		})
-
+	for i, path := range foldersWithPOMStringSlice {
+		log.Debug(strconv.Itoa(i) + " Detecting artifacts in folder '" + path + "'")
+		sb, err := pomDirs(path)
 		if err != nil {
 			return err
 		}
-
-		multipartString := strings.TrimSuffix(s.String(), ",")
-		url := n.URL + "/service/rest/" + n.APIVersion + "/components?repository=" + n.Repository
-		log.WithFields(log.Fields{
-			"multipart": multipartString,
-			"url":       url,
-		}).Debug("URL")
-
-		u := mp.Upload{URL: url, Username: n.User, Password: n.Pass}
-		if err := u.MultipartUpload(multipartString); err != nil {
+		log.Debug(strconv.Itoa(i) + " Upload '" + sb.String() + "'")
+		if err := n.multipartUpload(sb); err != nil {
 			return err
 		}
 	}
