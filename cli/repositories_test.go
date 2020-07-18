16f20d9311924c46eea941ed9e3a4b2e88dc6941
030 <chocolatey030@gmail.com>
2020-07-18 22:37:45 +0200

    [GH-137] Increased code coverage.

--- cli/repositories_test.go
+++ cli/repositories_test.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRepositoryNamesJSON(t *testing.T) {
@@ -12,4 +14,19 @@ func TestRepositoryNamesJSON(t *testing.T) {
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: '%v'. Actual: '%v'", expected, actual)
 	}
+}
+
+func TestCountRepositories(t *testing.T) {
+	assert.Nil(t, n.CountRepositories())
+	assert.EqualError(t, nErrAuth.CountRepositories(), testNexusAuthError)
+}
+
+func TestRepositoryNames(t *testing.T) {
+	assert.Nil(t, n.RepositoryNames())
+	assert.EqualError(t, nErrAuth.RepositoryNames(), testNexusAuthError)
+}
+
+func TestDownloads(t *testing.T) {
+	assert.Nil(t, n.Downloads(".*"))
+	assert.EqualError(t, nErrAuth.Downloads(".*"), testNexusAuthError)
 }
