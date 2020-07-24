25ae274f2d1ce9e6342a2fb4cae48d0abaf98b9d
ben <chocolatey030@gmail.com>
2020-07-24 20:17:01 +0200

    [GH-131] Allow definition of n3drUser and n3drPass in config file as well.

--- cmd/backup.go
+++ cmd/backup.go
@@ -5,7 +5,6 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var regex string
@@ -16,19 +15,13 @@ var backupCmd = &cobra.Command{
 	Short: "Backup all artifacts from a Nexus3 repository",
 	Long: `Use this command in order to backup all artifacts that
 reside in a certain Nexus3 repository`,
-	PreRun: func(cmd *cobra.Command, args []string) {
-		if err := viper.BindPFlag("n3drPass", rootCmd.Flags().Lookup("n3drPass")); err != nil {
-			log.Fatal(err)
-		}
-		enableDebug()
-	},
 	Run: func(cmd *cobra.Command, args []string) {
 		dir, err := cli.TempDownloadDir()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		n := cli.Nexus3{URL: n3drURL, User: n3drUser, Pass: viper.GetString("n3drPass"), Repository: n3drRepo, APIVersion: apiVersion, ZIP: zip, ZipName: zipName}
+		n := cli.Nexus3{URL: n3drURL, User: n3drUser, Pass: n3drPass, Repository: n3drRepo, APIVersion: apiVersion, ZIP: zip, ZipName: zipName}
 		if err := n.StoreArtifactsOnDisk(dir, regex); err != nil {
 			log.Fatal(err)
 		}
@@ -42,11 +35,11 @@ reside in a certain Nexus3 repository`,
 
 func init() {
 	backupCmd.PersistentFlags().StringVarP(&n3drRepo, "n3drRepo", "r", "", "nexus3 repository")
+	backupCmd.Flags().StringVarP(&regex, "regex", "x", ".*", "only download artifacts that match a regular expression, e.g. 'some/group42'")
 
 	if err := backupCmd.MarkPersistentFlagRequired("n3drRepo"); err != nil {
 		log.Fatal(err)
 	}
 
 	rootCmd.AddCommand(backupCmd)
-	backupCmd.Flags().StringVarP(&regex, "regex", "x", ".*", "only download artifacts that match a regular expression, e.g. 'some/group42'")
 }
