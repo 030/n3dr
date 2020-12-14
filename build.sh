e2d1ed7af37eca62ec671d876de65bd611c15591
ben <chocolatey030@gmail.com>
2020-12-14 14:05:23 +0100

    [#134] Integration test archetype-catalog.xml.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 TRAVIS_TAG="${TRAVIS_TAG:-local}"
 SHA512_CMD="${SHA512_CMD:-sha512sum}"
-DELIVERABLE="${DELIVERABLE:-n3dr}"
+export DELIVERABLE="${DELIVERABLE:-n3dr}"
 
 echo "TRAVIS_TAG: '$TRAVIS_TAG' DELIVERABLE: '$DELIVERABLE'"
 go build -ldflags "-X github.com/030/n3dr/cmd.Version=${TRAVIS_TAG}" -o "${DELIVERABLE}"
