2f29cc5bc0165b10871e998e44351a336e6c167e
ben <chocolatey030@gmail.com>
2021-01-31 21:35:36 +0100

    [GH-175] Create release using Github actions.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
-TRAVIS_TAG="${TRAVIS_TAG:-local}"
+GITHUB_TAG="${GITHUB_TAG:-local}"
 SHA512_CMD="${SHA512_CMD:-sha512sum}"
 export DELIVERABLE="${DELIVERABLE:-n3dr}"
 
-echo "TRAVIS_TAG: '$TRAVIS_TAG' DELIVERABLE: '$DELIVERABLE'"
-go build -ldflags "-X github.com/030/n3dr/cmd.Version=${TRAVIS_TAG}" -o "${DELIVERABLE}"
+echo "GITHUB_TAG: '$GITHUB_TAG' DELIVERABLE: '$DELIVERABLE'"
+go build -ldflags "-X github.com/030/n3dr/cmd.Version=${GITHUB_TAG}" -o "${DELIVERABLE}"
 $SHA512_CMD "${DELIVERABLE}" > "${DELIVERABLE}.sha512.txt"
 chmod +x "${DELIVERABLE}"
