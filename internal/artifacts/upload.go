e4f904760616aca4f08eb7de612262e9a0ed3d74
Alessandro Rossi <4215912+kubealex@users.noreply.github.com>
2021-03-12 10:29:45 +0100

    Adapted code to reflect changes

--- internal/artifacts/upload.go
+++ internal/artifacts/upload.go
@@ -69,13 +69,13 @@ func sbArtifact(sb *strings.Builder, path, ext, classifier string) error {
 func artifactTypeDetector(sb *strings.Builder, path string) error {
 	var err error
 
-	re := regexp.MustCompile(`^.*\/([a-z0-9\-]+)-([\d\.]+)(-[0-9a-z]{8,40})?-?([\w\.]+)?\.([a-z]+)$`)
+	re := regexp.MustCompile(`^.*\/([a-z0-9\-]+)-([\d\.]+)-?([\w\.\-]+)?\.([a-z]+)$`)
 	if re.Match([]byte(path)) {
 		result := re.FindAllStringSubmatch(path, -1)
 		log.Debugf("Artifact: '%v'", result[0][1])
-		log.Debugf("Version: '%v'", result[0][2]+result[0][3]+result[0][4])
-		classifier := result[0][5]
-		ext := result[0][6]
+		log.Debugf("Version: '%v'", result[0][2])
+		classifier := result[0][3]
+		ext := result[0][4]
 		err = sbArtifact(sb, path, ext, classifier)
 	} else {
 		log.Warningf("'%v' not an artifact", path)
