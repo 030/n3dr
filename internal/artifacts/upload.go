330ed729d548a14706074e19867ed0b35f453fe2
Alessandro Rossi <4215912+kubealex@users.noreply.github.com>
2021-03-12 10:15:53 +0100

    Adapting regex to maven conventions
    The artifact MUST respect the conventions:
    - Name should contain only lowercase,digit and hypens
    - Version contains only digits
    - Classifier can be any character

--- internal/artifacts/upload.go
+++ internal/artifacts/upload.go
@@ -69,7 +69,7 @@ func sbArtifact(sb *strings.Builder, path, ext, classifier string) error {
 func artifactTypeDetector(sb *strings.Builder, path string) error {
 	var err error
 
-	re := regexp.MustCompile(`^.*\/([\w\.-]+)-([\d\.]+)(-\d)?(-[0-9a-z]{8,40})?-([\w]+)?\.([a-z]+)$`)
+	re := regexp.MustCompile(`^.*\/([a-z0-9\-]+)-([\d\.]+)(-[0-9a-z]{8,40})?-?([\w\.]+)?\.([a-z]+)$`)
 	if re.Match([]byte(path)) {
 		result := re.FindAllStringSubmatch(path, -1)
 		log.Debugf("Artifact: '%v'", result[0][1])
