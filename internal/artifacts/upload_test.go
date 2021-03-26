083781e601327a4c1d28a0d57e3ff5efab1d2d1a
ben <chocolatey030@gmail.com>
2021-03-26 23:47:18 +0100

    [GH-193] Resolve GolangCI issues.

--- internal/artifacts/upload_test.go
+++ internal/artifacts/upload_test.go
@@ -26,9 +26,15 @@ func TestArtifactTypeDetector(t *testing.T) {
 	os.Setenv("N3DR_MAVEN_UPLOAD_REGEX_VERSION", `([\d\.-]+\d)`)
 	os.Setenv("N3DR_MAVEN_UPLOAD_REGEX_CLASSIFIER", `([a-zA-Z]+)?`)
 
-	artifactTypeDetector(&sb, "a/b/c.b.a/1.2.3/a-b-c-1.2.3-dataset.jar", false)
-	artifactTypeDetector(&sb, "d/e/f-e-d/4.5.6/d.e.f-4.5.6.pom", false)
-	artifactTypeDetector(&sb, "d/e/f-e-d/7.8.9-2/d.e.f-7.8.9-2.war", false)
+	if err := artifactTypeDetector(&sb, "a/b/c.b.a/1.2.3/a-b-c-1.2.3-dataset.jar", false); err != nil {
+		t.Error(err)
+	}
+	if err := artifactTypeDetector(&sb, "d/e/f-e-d/4.5.6/d.e.f-4.5.6.pom", false); err != nil {
+		t.Error(err)
+	}
+	if err := artifactTypeDetector(&sb, "d/e/f-e-d/7.8.9-2/d.e.f-7.8.9-2.war", false); err != nil {
+		t.Error(err)
+	}
 
 	assert.Equal(t, "maven2.asset0=@a/b/c.b.a/1.2.3/a-b-c-1.2.3-dataset.jar,maven2.asset0.extension=jar,maven2.asset0.classifier=dataset,maven2.asset1=@d/e/f-e-d/4.5.6/d.e.f-4.5.6.pom,maven2.asset1.extension=pom,maven2.asset2=@d/e/f-e-d/7.8.9-2/d.e.f-7.8.9-2.war,maven2.asset2.extension=war,", sb.String())
 }
