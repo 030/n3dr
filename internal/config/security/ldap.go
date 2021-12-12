0c49d7fc794bc98364c774cd52e97ca1f002b2aa
ben <chocolatey030@gmail.com>
2021-12-12 15:59:12 +0100

    [GH-232] Only download hosted artifacts.

---
+++ internal/config/security/ldap.go
@@ -0,0 +1,41 @@
+package security
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/030/n3dr/internal/goswagger/client/security_management_l_d_a_p"
+	"github.com/030/n3dr/internal/goswagger/models"
+	log "github.com/sirupsen/logrus"
+)
+
+type LDAPParams struct {
+	Security *Security
+	models.CreateLdapServerXo
+}
+
+func (l *LDAPParams) LDAP() error {
+	log.Info("configuring LDAP")
+
+	client := l.Security.Nexus3.Client()
+
+	createLdapServerParams := security_management_l_d_a_p.CreateLdapServerParams{Body: &l.CreateLdapServerXo}
+	createLdapServerParams.WithTimeout(time.Second * 30)
+
+	if _, err := client.SecurityManagementldap.CreateLdapServer(&createLdapServerParams); err != nil {
+		fmt.Println(err)
+		ldapCreated, errRegex := regexp.MatchString("status 400", err.Error())
+		if errRegex != nil {
+			return err
+		}
+		if ldapCreated {
+			log.Infof("ldap: '%s' has already been created", *l.CreateLdapServerXo.Name)
+			return nil
+		}
+
+		return fmt.Errorf("could not create ldap: '%v', err: '%v'", *l.CreateLdapServerXo.Name, err)
+	}
+	log.Infof("created the following repository: '%v'", *l.CreateLdapServerXo.Name)
+	return nil
+}
